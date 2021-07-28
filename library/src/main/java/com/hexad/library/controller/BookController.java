9b7c2be6fcd943325b4b4a276e1bb8d27d335e6e
nurersoyozyurek <nersoy88@gmail.com>
2021-07-28 14:08:03 +0200

    user can view books in library scenario book controller implementation

--- library/src/main/java/com/hexad/library/controller/BookController.java
+++ library/src/main/java/com/hexad/library/controller/BookController.java
@@ -1,6 +1,9 @@
 package com.hexad.library.controller;
 
 import com.hexad.library.controller.response.BookResponse;
+import com.hexad.library.mapper.BookMapper;
+import com.hexad.library.service.BookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +12,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/book")
+@RequiredArgsConstructor
 public class BookController {
+    private final BookService bookService;
+    private final BookMapper bookMapper = BookMapper.BOOK_MAPPER;
 
     @GetMapping
     public List<BookResponse> getAllBooksFromLibrary() {
-        return null;
+        return bookMapper.mapBookResponseListFrom(bookService.getAllBooks());
     }
 }
--- library/src/main/java/com/hexad/library/service/BookDto.java
+++ library/src/main/java/com/hexad/library/service/BookDto.java
@@ -1,12 +1,16 @@
 package com.hexad.library.service;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookDto {
     private UUID id;
     private String name;
